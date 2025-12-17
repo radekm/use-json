bde08e5478e5f1ee17588db0efd7753c04b56a1f
Radek Micek <radek.micek@gmail.com>
2025-12-17 12:02:37 +0100

    Rename library to ujson

--- test.sh
+++ test.sh
@@ -7,4 +7,4 @@ build_dir="$script_dir/build"
 mkdir -p "$build_dir"
 cd "$build_dir"
 
-c3c compile-test -O1 --libdir .. --lib use_json ../tests/tests.c3 -o tests
+c3c compile-test -O1 --libdir .. --lib ujson ../tests/tests.c3 -o tests
